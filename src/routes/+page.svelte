45dc314522db5316d85743a19fae65450857268e
josh mclain <joshmclain45@gmail.com>
2024-09-13 15:13:47 -0700

    tailwind, hero, fonts added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class='flex flex-col items-center justify-center h-screen bg-slate-800 px-4'>  
+  <h1 class='font-whisper text-[52px] text-white mt-[-120px]'>
+    Mystic Realms
+  </h1>
+  <p class="text-white text-center text-lg">For all your spiritual, metaphysical and occult needs.</p>
+</section>
+
+<style>
+  /* h1 {
+    font-size: 48px;
+  } */
+</style>
